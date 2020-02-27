f259b74ccb0e84208896d2e93d99d56457a44070
felibassx <felibass@gmail.com>
2020-02-27 11:17:07 -0300

    Docker, database

---
+++ src/database/database.service.ts
@@ -0,0 +1,27 @@
+// primero importamos el modulo de typeorm
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions';
+import { Configuration } from '../config/config.keys';
+
+// exportamos una constante con los tipos de conexión, esta para que nuestra app esté
+// preparada para cualquier conexión de base de datos
+export const databaseProviders = [
+    // preparamos un objeto de conexión para postgres
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        async useFactory(config: ConfigService) {
+            return {
+                ssl: true,
+                type: 'postgres' as 'postgres',
+                host: config.get(Configuration.HOST),
+                username: config.get(Configuration.USERNAME),
+                password: config.get(Configuration.PASSWORD),
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                migrations: [__dirname + '/migrations/*{.ts,.js}'],
+            } as ConnectionOptions;
+        }
+    }),
+];
